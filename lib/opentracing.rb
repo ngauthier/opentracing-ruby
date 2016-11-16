57a5442dc063204761ff1b73030357ab080a8756
Nick Gauthier <ngauthier@gmail.com>
2016-11-16 10:30:50 -0500

    inject and extract

--- lib/opentracing.rb
+++ lib/opentracing.rb
@@ -6,11 +6,54 @@ require "opentracing/span"
 require "opentracing/nil_tracer"
 
 module OpenTracing
+  FORMAT_TEXT_MAP = 1
+  FORMAT_BINARY = 2
+  FORMAT_RACK = 3
+
   class << self
     extend Forwardable
     # Global tracer to be used when OpenTracing.start_span is called
     attr_accessor :global_tracer
     def_delegator :global_tracer, :start_span
+
+
+    # Inject a span into the given carrier
+    # @param span_context [SpanContext]
+    # @param format [OpenTracing::FORMAT_TEXT_MAP, OpenTracing::FORMAT_BINARY, OpenTracing::FORMAT_RACK]
+    # @param carrier [Hash]
+    def inject(span_context, format, carrier)
+      case format
+      when OpenTracing::FORMAT_TEXT_MAP
+        inject_to_text_map(span_context, carrier)
+      when OpenTracing::FORMAT_BINARY
+        warn 'Binary inject format not yet implemented'
+      when OpenTracing::FORMAT_RACK
+        inject_to_rack(span_context, carrier)
+      else
+        warn 'Unknown inject format'
+      end
+    end
+
+    # Extract a span from a carrier
+    # @param operation_name [String]
+    # @param format [OpenTracing::FORMAT_TEXT_MAP, OpenTracing::FORMAT_BINARY, OpenTracing::FORMAT_RACK]
+    # @param carrier [Hash]
+    # @param tracer [Tracer] the tracer the span will be attached to (for finish)
+    # @return [Span]
+    def extract(operation_name, format, carrier, tracer)
+      case format
+      when OpenTracing::FORMAT_TEXT_MAP
+        extract_from_text_map(operation_name, carrier, tracer)
+      when OpenTracing::FORMAT_BINARY
+        warn 'Binary extract format not yet implemented'
+        nil
+      when OpenTracing::FORMAT_RACK
+        extract_from_rack(operation_name, carrier, tracer)
+      else
+        warn 'Unknown extract format'
+        nil
+      end
+    end
 
     # Convert a time to microseconds
     def micros(time)
@@ -22,6 +65,72 @@ module OpenTracing
     def guid
       @_rng ||= Random.new
       @_rng.bytes(8).unpack('H*')[0]
+    end
+
+    private
+
+    CARRIER_TRACER_STATE_PREFIX = 'ot-tracer-'.freeze
+    CARRIER_BAGGAGE_PREFIX = 'ot-baggage-'.freeze
+
+    # TODO(ngauthier@gmail.com) this shouldn't be in opentracer, but we use it
+    # on spans in lightstep tracer
+    DEFAULT_MAX_SPAN_RECORDS = 10_000
+
+    def inject_to_text_map(span_context, carrier)
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'spanid'] = span_context.id
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'traceid'] = span_context.trace_id unless span_context.trace_id.nil?
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'sampled'] = 'true'
+
+      span_context.baggage.each do |key, value|
+        carrier[CARRIER_BAGGAGE_PREFIX + key.to_s] = value.to_s
+      end
+    end
+
+    def extract_from_text_map(operation_name, carrier, tracer)
+      span = Span.new(
+        tracer: tracer,
+        operation_name: operation_name,
+        start_micros: OpenTracing.micros(Time.now),
+        child_of_id: carrier[CARRIER_TRACER_STATE_PREFIX + 'spanid'],
+        trace_id: carrier[CARRIER_TRACER_STATE_PREFIX + 'traceid'],
+        max_log_records: DEFAULT_MAX_SPAN_RECORDS
+      )
+
+      baggage = carrier.reduce({}) do |baggage, tuple|
+        key, value = tuple
+        if key.start_with?(CARRIER_BAGGAGE_PREFIX)
+          plain_key = key.to_s[CARRIER_BAGGAGE_PREFIX.length..key.to_s.length]
+          baggage[plain_key] = value
+        end
+        baggage
+      end
+      span.set_baggage(baggage)
+
+      span
+    end
+
+    def inject_to_rack(span_context, carrier)
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'spanid'] = span_context.id
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'traceid'] = span_context.trace_id unless span_context.trace_id.nil?
+      carrier[CARRIER_TRACER_STATE_PREFIX + 'sampled'] = 'true'
+
+      span_context.baggage.each do |key, value|
+        if key =~ /[^A-Za-z0-9\-_]/
+          # TODO: log the error internally
+          next
+        end
+        carrier[CARRIER_BAGGAGE_PREFIX + key.to_s] = value.to_s
+      end
+    end
+
+    def extract_from_rack(operation_name, env, tracer)
+      extract_from_text_map(operation_name, env.reduce({}){|memo, tuple|
+        raw_header, value = tuple
+        header = raw_header.gsub(/^HTTP_/, '').gsub("_", "-").downcase
+
+        memo[header] = value if header.start_with?(CARRIER_TRACER_STATE_PREFIX, CARRIER_BAGGAGE_PREFIX)
+        memo
+      }, tracer)
     end
   end
 end
--- test/opentracing_test.rb
+++ test/opentracing_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'net/http'
 
 class OpenTracingTest < Minitest::Test
   def test_micros
@@ -19,5 +20,103 @@ class OpenTracingTest < Minitest::Test
     span = Minitest::Mock.new
     tracer.expect(:start_span, span, ["span"])
     span = OpenTracing.start_span("span")
+  end
+
+  def test_inject_text_map
+    context = OpenTracing::SpanContext.new(id: "id", trace_id: "trace_id", baggage: {key: :value})
+    carrier = {}
+    OpenTracing.inject(context, OpenTracing::FORMAT_TEXT_MAP, carrier)
+    assert_equal({
+      "ot-tracer-spanid" => "id",
+      "ot-tracer-traceid" => "trace_id",
+      "ot-tracer-sampled" => "true",
+      "ot-baggage-key" => "value"
+    }, carrier)
+  end
+
+  def test_inject_binary
+    assert_warn "Binary inject format not yet implemented\n" do
+      OpenTracing.inject(nil, OpenTracing::FORMAT_BINARY, nil)
+    end
+  end
+
+  def test_inject_rack
+    context = OpenTracing::SpanContext.new(id: "id", trace_id: "trace_id", baggage: {
+      "key" => "value",
+      "Invalid%^&*%" => "dropped"
+    })
+    carrier = Net::HTTP::Post.new("/")
+    OpenTracing.inject(context, OpenTracing::FORMAT_RACK, carrier)
+    assert_equal carrier["ot-tracer-spanid"], "id"
+    assert_equal carrier["ot-tracer-traceid"], "trace_id"
+    assert_equal carrier["ot-tracer-sampled"], "true"
+    assert_equal carrier["ot-baggage-key"], "value"
+    assert_nil carrier["ot-baggage-Invalid%^&*%"]
+    assert_nil carrier["ot-baggage-invalid%^&*%"]
+    assert_nil carrier["ot-baggage-invalid"]
+  end
+
+  def test_invalid_inject_format
+    assert_warn "Unknown inject format\n" do
+      OpenTracing.inject(nil, 9999, nil)
+    end
+  end
+
+  def test_extract_text_map
+    tracer = Minitest::Mock.new
+    carrier = {
+      "ot-tracer-spanid" => "id",
+      "ot-tracer-traceid" => "trace_id",
+      "ot-tracer-sampled" => "true",
+      "ot-baggage-key" => "value"
+    }
+    span = OpenTracing.extract("operation_name", OpenTracing::FORMAT_TEXT_MAP, carrier, tracer)
+    context = span.span_context
+    refute_nil context.id
+    assert_equal "id", span.tags[:parent_span_guid]
+    assert_equal "trace_id", context.trace_id
+    assert_equal "value", context.baggage["key"]
+  end
+
+  def test_extract_binary
+    assert_warn "Binary extract format not yet implemented\n" do
+      OpenTracing.extract(nil, OpenTracing::FORMAT_BINARY, nil, nil)
+    end
+  end
+
+  def test_extract_rack
+    tracer = Minitest::Mock.new
+    carrier = {
+      "OT_TRACER_SPANID" => "id",
+      "OT_TRACER_TRACEID" => "trace_id",
+      "OT_TRACER_SAMPLED" => "true",
+      "OT_BAGGAGE_KEY" => "value"
+    }
+    span = OpenTracing.extract("operation_name", OpenTracing::FORMAT_RACK, carrier, tracer)
+    context = span.span_context
+    refute_nil context.id
+    assert_equal "id", span.tags[:parent_span_guid]
+    assert_equal "trace_id", context.trace_id
+    assert_equal "value", context.baggage["key"]
+  end
+
+  def test_extract_unknown
+    assert_warn "Unknown extract format\n" do
+      OpenTracing.extract(nil, 999, nil, nil)
+    end
+  end
+
+  private
+
+  def assert_warn(msg, &block)
+    original_stderr = $stderr
+    begin
+      str = StringIO.new
+      $stderr = str
+      block.call
+      assert_equal msg, str.string
+    ensure
+      $stderr = original_stderr
+    end
   end
 end
